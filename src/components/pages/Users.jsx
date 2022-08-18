9f954cbb1284fd90de497864eb8f63fbd7547f94
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-08-18 20:13:04 +0900

    section6.35

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -23,9 +23,11 @@ export const Users = () => {
     <SContainer>
       <h2>ユーザ一覧</h2>
       <SearchInput />
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
@@ -35,4 +37,11 @@ const SContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
